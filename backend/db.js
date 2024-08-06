9c0cf61f7e15325f513dc744e3e25c742a845c3a
yashuvaishnav <yashurishu22@gmail.com>
2024-08-06 11:51:16 +0530

    change local host link

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = mongoose.connect(
-  "mongodb://localhost:27017/event-management"
+  process.env.mongoLimk
 );
 
 module.exports = { connection };
